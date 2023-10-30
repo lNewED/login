d2872990cf7608ea99e0a7c6b3561be8652a3459
Athiphong Hinoon <newhazao.4@gmail.com>
2023-10-30 16:41:52 +0700

    first commit

---
+++ checklogin.php
@@ -0,0 +1,17 @@
+<?php
+require 'Login.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $login = new Login('localhost', 'authuser', 'root', '');
+
+    if ($login->authenticate($username, $password)) {
+        echo "Login successful!";
+        // Redirect or set session variable here
+    } else {
+        echo "Login failed!";
+    }
+}
+?>
